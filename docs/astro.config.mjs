4aa8e011070c696023a70f739b0f5b7d1620ae93
Michael Feher <michael.feher@algorand.foundation>
2024-07-03 11:32:33 -0400

    docs: update base url

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://liquid-auth.algorand.foundation',
+  base: '/liquid-auth',
   integrations: [
     starlight({
       title: 'Liquid\nAuth',
